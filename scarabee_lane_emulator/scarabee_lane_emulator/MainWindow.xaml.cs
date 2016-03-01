0a60ea8adc4e26e059df110ef6ce325c5d4dd50b
Sevak Mardirosian <sevak.mardirosian@office.scarabee.nl>
2016-03-01 15:27:32 +0100

    changs

--- scarabee_lane_emulator/scarabee_lane_emulator/MainWindow.xaml.cs
+++ scarabee_lane_emulator/scarabee_lane_emulator/MainWindow.xaml.cs
@@ -12,17 +12,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Snap7;
+using S7.Net;
 
 namespace scarabee_lane_emulator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    
     public partial class MainWindow : Window
     {
+
+      //  Dim Buffer(65536) as byte
+
         public MainWindow()
         {
             InitializeComponent();
+           // Client = new S7Client();
+
+            if (IntPtr.Size == 4)
+            {
+                //Text = 
+            }
+            else
+            {
+                 //Text = Text + " - Running 64 bit Code"
+            }
+
+        }
+
+        private void ConnectBtn_Click()
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
