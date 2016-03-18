7f63958f114405c5ace128d04adc1453a183e278
Sevak Mardirosian <sevak.mardirosian@office.scarabee.nl>
2016-03-18 10:54:37 +0100

    changes in folder structure

---
+++ Demo_console_application/ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Snap7;
+
+
+namespace ConsoleApplication2
+{   
+
+    class Program
+    {
+        static S7Client client = null;
+
+        static void Main(string[] args)
+        {
+                S7Client client = new S7Client();
+                int res = client.ConnectTo("192.168.2.16", 0, 0);
+         
+                byte[] buffer = new byte[1];
+
+                buffer[0] = 0;
+                           
+                res = client.WriteArea(S7Client.S7AreaPA, 0, 8, 1, S7Client.S7WLBit, buffer);
+        }
+    }
+}
