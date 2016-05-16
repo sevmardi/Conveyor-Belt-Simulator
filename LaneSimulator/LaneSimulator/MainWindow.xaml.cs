76facf12395c744988e1619ae26f51648b3cfdb2
Sevak .M <s1079770@student.hsleiden.nl>
2016-05-16 11:05:59 +0200

    new implmentations

--- LaneSimulator/LaneSimulator/MainWindow.xaml.cs
+++ LaneSimulator/LaneSimulator/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using LaneSimulator.PLC;
 using LaneSimulator.Views;
 
 namespace LaneSimulator
@@ -55,6 +45,88 @@ namespace LaneSimulator
             else
                 this.Close();
         }
+
+
+
+        private void btnRest_Click(object sender, RoutedEventArgs e)
+        {
+          //  PlcCalls.ResetBtn();
+        }
+
+        private void btnStopSystem_Click(object sender, RoutedEventArgs e)
+        {
+            //PLC.StopBtnInput();
+
+            //var sb1 = FindResource("Storyboard1") as Storyboard;
+            //if (sb1 != null) sb1.Pause();
+            //var sb2 = FindResource("Storyboard2") as Storyboard;
+            //if (sb2 != null) sb2.Pause();
+            //var sb3 = FindResource("Storyboard3") as Storyboard;
+            //if (sb3 != null) sb3.Pause();
+            //var sb4 = FindResource("Storyboard4") as Storyboard;
+            //if (sb4 != null) sb4.Pause();
+
+
+        }
+
+        private void StartAnimationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ////https://msdn.microsoft.com/en-us/library/cc295328.aspx
+
+            var sub1 = FindResource("Storyboard1") as Storyboard;
+
+            if (sub1 != null)
+            {
+                //Storyboard.SetTargetName(sub1, Tray);
+                sub1.Begin();
+                //CreateNewObject();
+
+                // SetMotorOnInSectionA();
+            }
+
+
+
+        }
+
+
+
+        private void SetMotorOnInSectionA()
+        {
+           // _sectionA.Executor();
+        }
+
+        private void Timeline_OnCompleted(object sender, EventArgs e)
+        {
+            var thing = this.FindResource("Storyboard3");
+
+            var OtherSB = (Storyboard)thing;
+            OtherSB.Begin();
+        }
+
+        private void Timeline_OnCompleted4(object sender, EventArgs e)
+        {
+
+            var thing = this.FindResource("Storyboard4");
+
+            var OtherSB = (Storyboard)thing;
+            OtherSB.Begin();
+        }
+
+
+        private void MyStoryboardCompleted(object sender, EventArgs e)
+        {
+            var thing = this.FindResource("Storyboard2");
+
+            var OtherSB = (Storyboard)thing;
+            OtherSB.Begin();
+        }
+
+        private void btnStartSystem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
 
     }
 }
