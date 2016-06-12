bdc35ef65e690e8812d94b36727d67b2f68398c1
Sevak .M <s1079770@student.hsleiden.nl>
2016-06-12 14:56:42 +0200

    small changes

--- Test_WPF/WpfApplication3/MainWindow.xaml.cs
+++ Test_WPF/WpfApplication3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -74,6 +75,8 @@ namespace WpfApplication3
             });
 
         }
+
+        
 
     }
 }
