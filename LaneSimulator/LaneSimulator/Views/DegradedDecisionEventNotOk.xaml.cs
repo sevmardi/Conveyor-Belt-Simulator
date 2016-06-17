94c8107631a980c5d1a7540d9e6d731238b3bfb7
Sevak Mardirosian <sevak.mardirosian@office.scarabee.nl>
2016-06-17 13:59:12 +0200

    new feature - > degraded mdoe

---
+++ LaneSimulator/LaneSimulator/Views/DegradedDecisionEventNotOk.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LaneSimulator.Views
+{
+    /// <summary>
+    /// Interaction logic for DegradedDecisionEventNotOk.xaml
+    /// </summary>
+    public partial class DegradedDecisionEventNotOk : UserControl
+    {
+        public DegradedDecisionEventNotOk()
+        {
+            InitializeComponent();
+        }
+
+
+        private void NotOk_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
